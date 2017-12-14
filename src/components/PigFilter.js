a4500b0e3b98dca6e19c8488b387553761f423d2
github username <github email address>
2017-12-14 13:11:18 -0500

    changes small

--- src/components/PigFilter.js
+++ src/components/PigFilter.js
@@ -6,12 +6,17 @@ export default class PigFilter extends React.Component {
       <div>
         <form action="">
           <h4>This is the filter</h4>
+          <div className="field">
+            Greasy pigs?
           <input
             onChange={this.props.filterGreasy}
             type="checkbox"
             value={this.props.isGreasy}
+            className="ui checkbox"
           />
-          <select onChange={this.props.changeFilter}>
+          </div>
+          <br/>
+          <select onChange={this.props.changeFilter} className="ui compact selection dropdown">
             <option value="name">By Name</option>
             <option value="weight">By Weight</option>
           </select>
